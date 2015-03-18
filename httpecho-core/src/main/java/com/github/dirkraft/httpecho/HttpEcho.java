c738be415fc42fee0b84eda9bcea154cb53e9332
Jason Dunkelberger (dirkraft) <dirkraft@gmail.com>
2015-03-18 03:57:20 +0000

    add text/html format and filter heroku headers, unless allHeaders=true is given

--- httpecho-core/src/main/java/com/github/dirkraft/httpecho/HttpEcho.java
+++ httpecho-core/src/main/java/com/github/dirkraft/httpecho/HttpEcho.java
@@ -2,12 +2,11 @@ package com.github.dirkraft.httpecho;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import org.apache.http.Header;
 import org.apache.http.HeaderElement;
 import org.apache.http.message.BasicHeader;
-import org.apache.http.message.BasicLineParser;
-import org.apache.http.message.LineParser;
-import org.apache.http.util.CharArrayBuffer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vertx.java.core.Handler;
@@ -21,6 +20,7 @@ import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -29,11 +29,26 @@ public class HttpEcho implements Runnable, Handler<HttpServerRequest> {
 
     private static final Logger LOG = LoggerFactory.getLogger(HttpEcho.class);
 
-    static final LineParser LINE_PARSER = new BasicLineParser();
     static final ObjectMapper MAPPER = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 
-    static final Map<String, Function<Object, String>> SERIALIZERS = Collections.unmodifiableMap(Stream.of(
+    /**
+     * Heroku does not seem to expose any way to disable forwarding headers. So we'll just have to filter them out
+     * and hope the original caller didn't include any such colliding headers.
+     */
+    static final Set<String> HEROKU_HEADERS = Stream.of(
+            "X-Request-Id",
+            "X-Forwarded-For",
+            "X-Forwarded-Proto",
+            "X-Forwarded-Port",
+            "Via",
+            "Connect-Time",
+            "X-Request-Start",
+            "Total-Route-Time"
+    ).collect(Collectors.toSet());
+
+    static final Map<String, Function<Map<String, Object>, String>> SERIALIZERS = Collections.unmodifiableMap(Stream.of(
             new Entry("text/plain", Object::toString),
+            new Entry("text/html", TextHtml::serialize),
             new Entry("application/json", (o) -> Annoyed.sh(() -> MAPPER.writeValueAsString(o)).get())
     ).collect(Collectors.toMap((s) -> s.name, (s) -> s.serializer)));
 
@@ -63,33 +78,8 @@ public class HttpEcho implements Runnable, Handler<HttpServerRequest> {
     public void handle(HttpServerRequest event) {
         try {
 
-            Map<String, Object> summary = new LinkedHashMap<>();
-
-            summary.put("version", event.version());
-            summary.put("method", event.method());
-            summary.put("uri", event.uri());
-            summary.put("absoluteURI", event.absoluteURI());
-            summary.put("path", event.path());
-            summary.put("query", event.query());
-            summary.put("params", event.params());
-            summary.put("headers", event.headers().entries());
-            summary.put("remoteAddress", event.remoteAddress());
-
-            Optional<String> acceptOpt = Optional.ofNullable(event.headers().get(HttpHeaders.ACCEPT));
-            Function<Object, String> serializer = null;
-            if (acceptOpt.isPresent()) {
-                Header acceptHeader = new BasicHeader(HttpHeaders.ACCEPT.toString(), acceptOpt.get());
-                for (int i = 0; i < acceptHeader.getElements().length && serializer == null; i++) {
-                    HeaderElement headerElement = acceptHeader.getElements()[i];
-                    String accept = headerElement.getName();
-                    serializer = SERIALIZERS.get(accept);
-                }
-            }
-            if (serializer == null) {
-                serializer = SERIALIZERS.get("text/plain");
-            }
-
-            String serialized = serializer.apply(summary);
+            Map<String, Object> summary = summarize(event);
+            String serialized = serialize(event, summary);
             event.response().end(serialized);
 
         } finally {
@@ -97,11 +87,53 @@ public class HttpEcho implements Runnable, Handler<HttpServerRequest> {
         }
     }
 
+    Map<String, Object> summarize(HttpServerRequest event) {
+        boolean allHeaders = "true".equalsIgnoreCase(event.params().get("allHeaders"));
+        final Predicate<Map.Entry<String, String>> headersPredicate;
+        if (allHeaders) {
+            headersPredicate = (e) -> true;
+        } else {
+            headersPredicate = (e) -> !HEROKU_HEADERS.contains(e.getKey());
+        }
+
+        Map<String, Object> summary = new LinkedHashMap<>();
+
+        summary.put("version", event.version());
+        summary.put("method", event.method());
+        summary.put("uri", event.uri());
+        summary.put("absoluteURI", event.absoluteURI());
+        summary.put("path", event.path());
+        summary.put("query", event.query());
+        summary.put("params", event.params());
+        summary.put("headers", Iterables.filter(event.headers().entries(), headersPredicate));
+        summary.put("remoteAddress", event.remoteAddress());
+
+        return summary;
+    }
+
+    String serialize(HttpServerRequest event, Map<String, Object> summary) {
+        Optional<String> acceptOpt = Optional.ofNullable(event.headers().get(HttpHeaders.ACCEPT));
+        Function<Map<String, Object>, String> serializer = null;
+        if (acceptOpt.isPresent()) {
+            Header acceptHeader = new BasicHeader(HttpHeaders.ACCEPT.toString(), acceptOpt.get());
+            for (int i = 0; i < acceptHeader.getElements().length && serializer == null; i++) {
+                HeaderElement headerElement = acceptHeader.getElements()[i];
+                String accept = headerElement.getName();
+                serializer = SERIALIZERS.get(accept);
+            }
+        }
+        if (serializer == null) {
+            serializer = SERIALIZERS.get("text/plain");
+        }
+
+        return serializer.apply(summary);
+    }
+
     static class Entry {
         final String name;
-        final Function<Object, String> serializer;
+        final Function<Map<String, Object>, String> serializer;
 
-        public Entry(String name, Function<Object, String> serializer) {
+        public Entry(String name, Function<Map<String, Object>, String> serializer) {
             this.name = name;
             this.serializer = serializer;
         }
---
+++ httpecho-core/src/main/java/com/github/dirkraft/httpecho/TextHtml.java
@@ -0,0 +1,41 @@
+package com.github.dirkraft.httpecho;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
+import java.util.Objects;
+
+public class TextHtml {
+
+    public static final String TEMPLATE = Annoyed.shh(() ->
+            Resources.toString(Resources.getResource("template.html"), Charsets.UTF_8));
+    public static final String TOKEN_TBODY = "TOKEN_TBODY";
+
+    public static String serialize(Map<String, Object> m) {
+        StringWriter html = new StringWriter();
+        PrintWriter p = new PrintWriter(html);
+
+        for (Map.Entry<String, Object> e : m.entrySet()) {
+            final String value;
+            if (e.getValue() instanceof Iterable) {
+                StringWriter inner = new StringWriter();
+                PrintWriter pInner = new PrintWriter(inner);
+                for (Object o : (Iterable) e.getValue()) {
+                    pInner.println("<ul>");
+                    pInner.format("<li>%s</li>%n", Objects.toString(o));
+                    pInner.println("</ul>");
+                }
+                value = inner.toString();
+            } else {
+                value = Objects.toString(e.getValue());
+            }
+            p.format("      <tr><td>%s</td><td>%s</td></tr>%n", e.getKey(), value);
+        }
+
+        return TEMPLATE.replace(TOKEN_TBODY, html.toString());
+    }
+
+}
